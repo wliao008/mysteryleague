dbf83391d4895189f248ce982504a49cc0480039
Wei Liao <wliao@fedora13.(none)>
2011-05-23 16:00:20 -0400

    added base template and model

--- src/controllers/hello.py
+++ src/controllers/hello.py
@@ -1,10 +1,15 @@
+import os
 from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
+
+VIEWS_PATH = '../views/'
 
 class MainPage(webapp.RequestHandler):
   def get(self):
-    self.response.headers['Content-Type'] = 'text/plain'
-    self.response.out.write('Hello!')
+    path = os.path.join(os.path.dirname(VIEWS_PATH), 'index.html')
+    model = {'name': 'man', 'path': path, }
+    self.response.out.write(template.render(path, model))
 
 application = webapp.WSGIApplication([('/', MainPage)], debug=True)
 
---
+++ src/models/model.py
@@ -0,0 +1,8 @@
+from google.appengine.ext import db
+from google.appengine.ext.db import polymodel
+
+class Item(polymodel.PolyModel):
+  title = db.StringProperty(required=True)
+
+class Article(Item):
+  Author = db.StringProperty()
