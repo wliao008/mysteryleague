c543cd09ba11482ab67488009ec22a605318fe26
Wei Liao <wliao008@gmail.com>
2011-05-22 21:36:43 -0400

    rm some django files

--- src/settings.py
+++ src/settings.py
@@ -1,5 +1,8 @@
 # Django settings for src project.
 
+import os
+
+ROOT_PATH = os.path.dirname(__file__)
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -81,6 +84,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    ROOT_PATH + '/templates'    
 )
 
 INSTALLED_APPS = (
@@ -93,4 +97,5 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'src.mainsite'
 )
