958dd36688d95ec074ea3a14142a6af27f7a037b
wliao008 <wliao008@gmail.com>
2011-05-24 16:05:52 -0400

    more experimental with appengine model

--- src/controllers/hello.py
+++ src/controllers/hello.py
@@ -13,7 +13,14 @@ class MainPage(webapp.RequestHandler):
     model = {'name': 'man', 'path': path, 'articles': articles, }
     self.response.out.write(template.render(path, model))
 
-application = webapp.WSGIApplication([('/', MainPage)], debug=True)
+class ArticleDetail(webapp.RequestHandler):
+  def get(self, cat, key):
+    path = os.path.join(os.path.dirname(VIEWS_PATH), 'articledetail.html')
+    model = {'cat': cat, 'key': key}
+    self.response.out.write(template.render(path, model))
+
+
+application = webapp.WSGIApplication([('/', MainPage), (r'/(detail)/(\w+)', ArticleDetail)], debug=True)
 
 def main():
   run_wsgi_app(application)
