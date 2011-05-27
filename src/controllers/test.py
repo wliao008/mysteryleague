4a9f234dddd23f43b54689dc116319f68482a222
Wei Liao <wliao008@gmail.com>
2011-05-27 16:10:12 -0400

    trying out the google user service

--- src/controllers/test.py
+++ src/controllers/test.py
@@ -1,23 +1,36 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.api import users
 import models.model
 
 class Test(webapp.RequestHandler):
-
     def get(self):
-        create()
+        create(100)
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.out.write('Test')
         self.response.out.write('<p>Created')
 
 
-app = webapp.WSGIApplication([('/test/*', Test)], debug=True)
+class TestUser(webapp.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            self.response.out.write('test user: ' + user.nickname())
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
 
-def create():
-    article = models.model.Article(title="Test title", author="wliao")
-    article.summary = "this is a test"
-    article.content = "this is the content of the article"
-    article.put()    
+app = webapp.WSGIApplication([('/test/*', Test), ('/testuser/*', TestUser)], debug=True)
+
+def create(count):
+    for i in range(0,count,1):
+        title = "Test title " + str(i)
+        author = "wliao"
+        summary = "this is a test " + str(i)
+        content = "this is the content of the article"
+        article = models.model.Article(title=title, author=author,
+                               summary=summary, content=content)
+
+        article.put()    
 
 def main():
     run_wsgi_app(app)
--- src/models/model.py
+++ src/models/model.py
@@ -3,8 +3,9 @@ from google.appengine.ext.db import polymodel
 
 class Item(polymodel.PolyModel):
     title = db.StringProperty(required=True)
-    summary = db.StringProperty()
-    content = db.TextProperty()
+    summary = db.StringProperty(required=True)
+    content = db.TextProperty(required=True)
+    createDate = db.DateTimeProperty(auto_now_add=True)
 
 class Article(Item):
     author = db.StringProperty()
