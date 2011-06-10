58dd671188d31d5ab3448808197df224597555a1
wliao008 <wliao008@gmail.com>
2011-06-09 23:23:43 -0400

    Updating model

--- src/models/model.py
+++ src/models/model.py
@@ -3,9 +3,16 @@ from google.appengine.ext.db import polymodel
 
 class Item(polymodel.PolyModel):
     title = db.StringProperty(required=True)
-    summary = db.StringProperty(required=True)
-    content = db.TextProperty(required=True)
+    item_type = db.IntegerProperty(required=True)
+    summary = db.StringProperty()
+    content_html = db.TextProperty()
+    content_wmd = db.TextProperty()
     createDate = db.DateTimeProperty(auto_now_add=True)
+    hits = db.IntegerProperty(default=0)
+    is_reviewable = db.BooleanProperty()
+    is_ratable = db.BooleanProperty()
+    rating = db.RatingProperty()
+    user = db.UserProperty()
 
 class Article(Item):
-    author = db.StringProperty()
+    original_author = db.StringProperty()
