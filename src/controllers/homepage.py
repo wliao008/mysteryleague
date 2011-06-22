51d725ee418d86e1447aacac6923d3e285f65bab
wliao008 <wliao008@gmail.com>
2011-06-22 11:37:06 -0400

    Added .gitignore file
    To ignore *.pyc and any bulkloading log files.

--- src/controllers/homepage.py
+++ src/controllers/homepage.py
@@ -19,7 +19,15 @@ class MainPage(webapp.RequestHandler):
         model = {'name': 'man', 'path': path, "count": count, "page_size": PAGE_SIZE, 'articles': articles.fetch(PAGE_SIZE, offset), }
         self.response.out.write(template.render(path, model))
 
-application = webapp.WSGIApplication([('/', MainPage), ('/page(\d+)', MainPage)], debug=True)
+class Login(webapp.RequestHandler):
+    def get(self):
+	path = os.path.join(os.path.dirname(VIEWS_PATH), 'login.html')
+	self.response.out.write(template.render(path, None))
+
+application = webapp.WSGIApplication([
+				('/', MainPage), 
+				('/page(\d+)', MainPage),
+				('/login', Login)], debug=True)
 
 def main():
     run_wsgi_app(application)
