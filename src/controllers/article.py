945a12bf45d3fe56283b7dc80ca1461a08763915
Wei Liao <wliao008@gmail.com>
2011-06-22 22:36:42 -0400

    Fixed indentations

--- src/controllers/article.py
+++ src/controllers/article.py
@@ -3,7 +3,7 @@ from google.appengine.ext.webapp import template
 from google.appengine.api import users
 import os
 import cgi
-import models.model
+from models import model
 
 VIEWS_PATH = os.path.join(os.path.dirname(__file__), '../views/')
 
@@ -18,18 +18,18 @@ class ArticleDetail(webapp.RequestHandler):
         login_msg = ""
         if not user:
             #login_url = users.create_login_url(self.request.uri)
-	    login_url = "/login?continue=" + self.request.uri
+            login_url = "/login?continue=" + self.request.uri
             login_msg = "Please <a href=" +  login_url + ">login</a> to leave comment ;)"
 
         model = {'cat': cat, 'article': article, 'user': user, 'login_url': login_url, 'login_msg': login_msg}
         self.response.out.write(template.render(path, model))
 
     def post(self):
-	#TODO: shouldn't retrieve the whole article object just to reference it,
-	#there should be a better way, research on how the foreign key work in appengine
+        #TODO: shouldn't retrieve the whole article object just to reference it,
+        #there should be a better way, research on how the foreign key work in appengine
         key = self.request.get('key')
         article = db.get(key)
-        review = models.model.Review()
+        review = model.Review()
         review.content_html = cgi.escape(self.request.get('content'))
         review.item = article
         review.put()
--- src/controllers/test.py
+++ src/controllers/test.py
@@ -1,7 +1,7 @@
 from google.appengine.ext import db, webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.api import users
-import models.model
+from models import model
 
 openIdProviders = (
     'Google.com/accounts/o8/id', # shorter alternative: "Gmail.com"
@@ -27,8 +27,8 @@ class TestUser(webapp.RequestHandler):
             self.response.out.write('Hello <em>%s</em>! [<a href="%s">sign out</a>]' % (
                 user.nickname(), users.create_logout_url(self.request.uri)))
         else:
-	    self.response.out.write('Hello world! Sign in at: ')
-	    #self.response.out.write('[<a href="%s">%s</a>]' % (users.create_login_url(federated_identity='Yahoo.com', dest_url='/'), 'Yahoo.com'))
+            self.response.out.write('Hello world! Sign in at: ')
+            #self.response.out.write('[<a href="%s">%s</a>]' % (users.create_login_url(federated_identity='Yahoo.com', dest_url='/'), 'Yahoo.com'))
             for p in openIdProviders:
                 p_name = p.split('.')[0] # take "AOL" from "AOL.com"
                 p_url = p.lower()        # "AOL.com" -> "aol.com"
@@ -39,7 +39,7 @@ class TestReview(webapp.RequestHandler):
         article = db.get('agl0dWlsaWNsdWJyCwsSBEl0ZW0YkQMM')
         user = db.get('agl0dWlsaWNsdWJyEAsSBFVzZXIiBuS5kOmYsww')
         for i in range(4):
-            review1 = models.model.Review()
+            review1 = model.Review()
             review1.subject="review " + str(i)
             review1.content_html="this is a review " + str(i)
             review1.item = article
@@ -56,9 +56,9 @@ def create():
     author = "wliao"
     summary = "this is a test"
     content = "this is the content of the article"
-    usr = models.model.User(email="wliao008@gmail.com",nickname="wliao")
+    usr = model.User(email="wliao008@gmail.com",nickname="wliao")
     usr.put()
-    article = models.model.Article(item_type=1,title=title, original_author=author,summary=summary, content_html=content, user=usr)
+    article = model.Article(item_type=1,title=title, original_author=author,summary=summary, content_html=content, user=usr)
 
     article.put()    
 
