2a317fd006a6adaef638429e7464ccc523e67da6
Wei Liao <wliao008@gmail.com>
2011-06-24 16:58:41 -0400

    Added pagination to the homepage
    The pagination script is pulled from https://github.com/wliao008/paging

--- src/controllers/main.py
+++ src/controllers/main.py
@@ -3,9 +3,10 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 import models.model
 import os
+import paging
 
 VIEWS_PATH = os.path.join(os.path.dirname(__file__), '../views/')
-PAGE_SIZE = 50
+PAGE_SIZE = 10
 
 class MainPage(webapp.RequestHandler):
     def get(self, pagenum=None):
@@ -16,7 +17,8 @@ class MainPage(webapp.RequestHandler):
         items.order("-created_date")
         offset = PAGE_SIZE * int(pagenum)
         count = items.count()
-        model = {'name': 'man', 'path': path, "count": count, "page_size": PAGE_SIZE, 'items': items.fetch(PAGE_SIZE, offset), }
+        links = paging.link(pagenum, count/PAGE_SIZE, 6, 2, 'prev', 'next', dummy)
+        model = {'name': 'man', 'path': path, "count": count, "page_size": PAGE_SIZE, 'items': items.fetch(PAGE_SIZE, offset), 'links': links}
         self.response.out.write(template.render(path, model))
 
 class Login(webapp.RequestHandler):
@@ -36,3 +38,6 @@ class Logout(webapp.RequestHandler):
     def get(self):
         url = users.create_logout_url("/")
         self.redirect(url)
+
+def dummy():
+    print 'dummy'
